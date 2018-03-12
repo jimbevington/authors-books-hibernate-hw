81ae70f0a23903f53f65442609d98d16175e9bb2
Jim Bevington <jim.bevington@hotmail.com>
2018-03-12 17:05:39 +0000

    added getAll method to DBHelper

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,6 +2,8 @@ import db.DBHelper;
 import models.Author;
 import models.Book;
 
+import java.util.List;
+
 public class Runner {
 
     public static void main(String[] args) {
@@ -9,14 +11,25 @@ public class Runner {
         Author author2 = new Author("Margaret", "Atwood");
 
         Book book1 = new Book("The Lion, The Witch & The Wardrobe", author1);
+        Book book3 = new Book("The Voyage of the Dawn Treader", author1);
         Book book2 = new Book("The Handmaids Tale", author2);
 
         DBHelper.save(author1);
         DBHelper.save(author2);
         DBHelper.save(book1);
         DBHelper.save(book2);
+        DBHelper.save(book3);
 
         author1.setFirstName("Arnold");
         DBHelper.update(author1);
+
+        List<Book> books = DBHelper.getAll("Book");
+        List<Author> authors = DBHelper.getAll("Author");
+
+        // for testing
+//        DBHelper.delete(book1);
+//        NOTE:-  found problem with Cascade Delete, couldn't delete the author without causing an error
+
+
     }
 }
--- src/main/java/db/DBHelper.java
+++ src/main/java/db/DBHelper.java
@@ -1,8 +1,11 @@
 package db;
 
+import org.hibernate.Hibernate;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class DBHelper {
 
@@ -40,7 +43,38 @@ public class DBHelper {
         }
     }
 
-//    update
+    public static void delete(Object object){
+        session = HibernateUtil.getSessionFactory().openSession();
+
+        try {
+            transaction = session.beginTransaction();
+            session.delete(object);
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+    }
+
+    public static <T> List<T> getAll(String className){
+        session = HibernateUtil.getSessionFactory().openSession();
+        List<T> results = null;
+
+        try {
+            transaction = session.beginTransaction();
+            String hql = "from " + className;
+            results = session.createQuery(hql).list();
+            transaction.commit();
+        } catch (HibernateException e) {
+            transaction.rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return results;
+    }
 //    getAll
 //    deleteAll
 //    delete
