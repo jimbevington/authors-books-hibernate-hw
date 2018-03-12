f404163de9c9aaba50ed33ce3f4616aae0a2ceb5
Jim Bevington <jim.bevington@hotmail.com>
2018-03-12 20:20:43 +0000

    refactored Runner file and added comments

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -14,36 +14,54 @@ public class Runner {
         Book book3 = new Book("The Voyage of the Dawn Treader", author1);
         Book book2 = new Book("The Handmaids Tale", author2);
 
+//        TEST SAVE
+
         DBHelper.save(author1);
         DBHelper.save(author2);
         DBHelper.save(book1);
         DBHelper.save(book2);
         DBHelper.save(book3);
 
+//        TEST UPDATE
+
         author1.setFirstName("Arnold");
         DBHelper.update(author1);
 
 
-// DELETE ALL TESTS
+//        TEST DELETE ALL
 
 //        DBHelper.deleteAll("Book");
 
-//          why can't I delete all Authors without deleting books?
+//          Why can't I delete all Authors without deleting Books first?
 //          Surely I should be able to delete an Author, taking all of their Books with it?
 
 //        DBHelper.deleteAll("Author");
 
+
+//        TEST DELETE OBJECT
+
+//        DBHelper.delete(book1);
+//        NOTE:-  found problem with Cascade Delete, couldn't delete the author without causing an error
+
+
+//        TEST GET ALL
+
         List<Book> books = DBHelper.getAll("Book");
         List<Author> authors = DBHelper.getAll("Author");
 
+
+//        TEST GET BOOKS BY AUTHOR
+
         List<Book> booksByAuthor = DBHelper.getBooksByAuthor(author1.getId());
+
+
+//        TEST FIND BY ID
 
         Author foundAuthor = DBHelper.findByID("Author", author1.getId());
         Book foundBook = DBHelper.findByID("Book", book1.getId());
+
+//        test will return null if Item not present
         Book noBook = DBHelper.findByID("Book", 12);
-        // for testing
-//        DBHelper.delete(book1);
-//        NOTE:-  found problem with Cascade Delete, couldn't delete the author without causing an error
 
 
 
